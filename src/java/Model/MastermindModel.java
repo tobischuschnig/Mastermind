e677a15927f76f3d626993a61f01f89065519e1c
tobischuschnig <tschuschnig@student.tgm.ac.at>
2013-11-03 16:20:20 +0100

    Model
    Model erster Funktionalitaeten

--- src/java/Model/MastermindModel.java
+++ src/java/Model/MastermindModel.java
@@ -4,10 +4,44 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author tobi
  */
 public class MastermindModel {
-    
+    private ArrayList<int[]> versuche;
+    private ArrayList<int[]> check;
+    private int[] zahlen;
+    public MastermindModel() {
+        zahlen = new int[4];
+        for (int i = 0 ; i < 4; i++) {
+            zahlen[1] = ((int) (Math.random()*8+1));
+	}
+    }
+    public ArrayList<int[]> check(int[] vers) {
+        int[] checkvers = new int[2];
+        checkvers[1] = checkFarbe(vers);
+        checkvers[2] = checkStelle(vers);
+        check.add(checkvers);
+        return check;
+    }
+    public int checkFarbe(int[] vers) {
+        int farbe = 0;
+        for(int i = 0; i<4;i++) {
+            for(int ii = 0; ii<4;ii++) {
+                if(zahlen[i]==vers[ii])farbe++;
+            }
+        }
+        return farbe;
+    }
+
+    private int checkStelle(int[] vers) {
+        int stelle = 0;
+        for(int i = 0;i<4;i++) {
+            if(vers[i]==zahlen[i])stelle++;
+        }
+        return stelle;
+    }
 }
